9496ddeda8b5a869163f16a716528797487f46f9
Alexander Quispe Alvi <alxnderq@gmail.com>
2019-09-25 18:40:54 -0500

    Diseño de pantalla principal

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,14 +1,23 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
     MatToolbarModule
   ],
   exports: [
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
     MatToolbarModule
   ]
 })
